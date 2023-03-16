e985c016f8ad072f77870db702401366eff26114
Khalil Khalil <khalil.t.khalil@live.com>
2023-03-16 12:00:13 -0500

    writes index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) + 1;
+
+const num3 = 29;
+const num4 = 5;
+const mod = 29 % 5;
+
+const max = Math.max(1,4,7,8,20);
